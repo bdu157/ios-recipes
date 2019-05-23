b4e678cec70ec299c9f81d1d00c023b0ca15b5cc
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-22 18:27:39 -0700

    set up MainViewController with container view

---
+++ Recipes/Recipes/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Dongwoo Pae on 5/22/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var SearchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func SearchTextFieldAction(_ sender: Any) {
+    }
+    
+}
